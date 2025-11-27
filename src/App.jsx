ffd828760e9d27f28ae975c73fcef4f85c89e8f0
rockerfeller9 <chukwunonsookoye9@gmail.com>
2025-11-26 20:06:21 -0700

    Fix file casing for LoanApplicationPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,21 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
-import theme from "./theme";
-import Navbar from "./components/Navbar";
 
-import LoginPage from "./pages/LoginPage.jsx";
-import RegisterPage from "./pages/RegisterPage.jsx";
-import DashboardPage from "./pages/DashboardPage.jsx";
-import LoanApplicationPage from "./pages/LoanApplicationPage.jsx";
-import AdminDashboardPage from "./pages/AdminDashboardPage.jsx";
-import ProfilePage from "./pages/ProfilePage.jsx";
-import ProjectInvestmentPage from "./pages/ProjectInvestmentPage.jsx";
-import PrivateRoute from "./components/PrivateRoute.jsx";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
+import LoanApplicationPage from "./pages/LoanApplicationPage";
+import NotificationPage from "./pages/NotificationPage";
 
 export default function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Navbar />
+    <ChakraProvider>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route 
-          path="/dashboard" 
-          element={
-            <PrivateRoute>
-              <DashboardPage />
-            </PrivateRoute>
-          } 
-        />
-        <Route 
-          path="/loans" 
-          element={
-            <PrivateRoute>
-              <LoanApplicationPage />
-            </PrivateRoute>
-          } 
-        />
-        <Route 
-          path="/profile" 
-          element={
-            <PrivateRoute>
-              <ProfilePage />
-            </PrivateRoute>
-          } 
-        />
-        <Route 
-          path="/investments" 
-          element={
-            <PrivateRoute>
-              <ProjectInvestmentPage />
-            </PrivateRoute>
-          } 
-        />
-        <Route 
-          path="/admin" 
-          element={
-            <PrivateRoute adminOnly>
-              <AdminDashboardPage />
-            </PrivateRoute>
-          } 
-        />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/loan-application" element={<LoanApplicationPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
       </Routes>
     </ChakraProvider>
   );
